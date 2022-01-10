e3251d62315d0ccce3e46f2f0dff72b87f95c13f
Macascript <jotaele.arrojo@gmail.com>
2022-01-10 02:17:34 +0100

    SQL corregido

--- CreateDatabase_query.sql
+++ CreateDatabase_query.sql
@@ -4,7 +4,7 @@ CREATE TABLE Video(
     ruta_aws VARCHAR(500) UNIQUE,
     id INT(11) PRIMARY KEY,
     fecha_subida DATETIME
-)
+) ENGINE = INNODB;
 
 CREATE TABLE Voto(
     usuario_id INT(11),
@@ -12,15 +12,16 @@ CREATE TABLE Voto(
     valor INT(1),
     id INT(11) PRIMARY KEY,
     UNIQUE(usuario_id,video_id)
-)
+) ENGINE = INNODB;
 
 CREATE TABLE Comentario(
     id INT(11) PRIMARY KEY,
     usuario_id INT(11),
     video_id INT(11),
     contenido VARCHAR(255),
-    comentario_padre_id INT(11) FOREIGN KEY REFERENCES Comentario(id)
-)
+    comentario_padre_id INT(11),
+    FOREIGN KEY(comentario_padre_id) REFERENCES Comentario(id)
+) ENGINE = INNODB;
 
 CREATE TABLE Usuario (
   id INT(11) NOT NULL,
@@ -31,17 +32,17 @@ CREATE TABLE Usuario (
   frase_recuperacion varchar(100) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY usuario_UN1(email),
-  UNIQUE KEY usuario_UN2 (nombre_usuario),
-)
+  UNIQUE KEY usuario_UN2 (nombre_usuario)
+) ENGINE = INNODB;
 
 CREATE TABLE Video_Tags (
   video_id INT(11) NOT NULL,
-  tag varchar(30) NOT NULL,
-)
+  tag varchar(30) NOT NULL
+) ENGINE = INNODB;
 
-ALTER TABLE Video ADD CONSTRAINT FK_Video_Usuario FOREIGN KEY usuario_id REFERENCES Usuario(id);
-ALTER TABLE Voto ADD CONSTRAINT FK_Voto_Usuario FOREIGN KEY usuario_id REFERENCES Usuario(id);
-ALTER TABLE Voto ADD CONSTRAINT FK_Voto_Video FOREIGN KEY video_id REFERENCES Video(id);
-ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Video FOREIGN KEY video_id REFERENCES Video(id);
-ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Usuario FOREIGN KEY usuario_id REFERENCES Usuario(id);
-ALTER TABLE Video_Tags ADD CONSTRAINT FK_Video_Tags_Video FOREIGN KEY video_id REFERENCES Video(id);
+ALTER TABLE Video ADD CONSTRAINT FK_Video_Usuario FOREIGN KEY(usuario_id) REFERENCES Usuario(id);
+ALTER TABLE Voto ADD CONSTRAINT FK_Voto_Usuario FOREIGN KEY(usuario_id) REFERENCES Usuario(id);
+ALTER TABLE Voto ADD CONSTRAINT FK_Voto_Video FOREIGN KEY(video_id) REFERENCES Video(id);
+ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Video FOREIGN KEY(video_id) REFERENCES Video(id);
+ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Usuario FOREIGN KEY(usuario_id) REFERENCES Usuario(id);
+ALTER TABLE Video_Tags ADD CONSTRAINT FK_Video_Tags_Video FOREIGN KEY(video_id) REFERENCES Video(id);
