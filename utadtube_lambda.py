4126aa94ac91eecf3377da1aae2dd61934e1641c
Macascript <jotaele.arrojo@gmail.com>
2022-01-11 12:21:44 +0100

    Implementados en lambda: subir, nuevoVideo (el frontend tendrá que llamar 2 veces para una subida de video, una a subir y otra a nuevoVideo), y comentar.

--- utadtube_lambda.py
+++ utadtube_lambda.py
@@ -58,6 +58,7 @@ def login(user,password):
             conn.commit()
             if ok > 0:
                 id = cur.fetchone()[0]
+                body["id"] = cur.fetchone()[0]
                 body["nombreUsuario"] = cur.fetchone()[1]
                 cur.execute("select nombre,descripcion,rutaAWS,fechaSubida,ultimaModificacion from Video where usuarioId="+id+" order by fechaSubida")
                 conn.commit()
@@ -76,6 +77,7 @@ def login(user,password):
             
     except pymysql.MySQLError as e:    
         print (e)
+        body["redirectPage"] = urlbase+"error.html"
     return {
         'statusCode': 200,
         'headers': { 'Access-Control-Allow-Origin' : '*' },
@@ -84,7 +86,7 @@ def login(user,password):
 
 def registrarse(nombreUsuario,email,nombreCompleto,contrasenya,fraseRecuperacion):
     conn = connect()
-    body = {"redirectPage": urlbase+"login.html"}
+    body = {}
     try:
         with conn.cursor() as cur:
             ok = cur.execute("select id,nombreUsuario from Usuario where nombreUsuario="+nombreUsuario)
@@ -111,6 +113,67 @@ def registrarse(nombreUsuario,email,nombreCompleto,contrasenya,fraseRecuperacion
             
     except pymysql.MySQLError as e:    
         print (e)
+        body["redirectPage"] = urlbase+"error.html"
+    return {
+        'statusCode': 200,
+        'headers': { 'Access-Control-Allow-Origin' : '*' },
+        'body' : json.dumps(body)
+    }
+
+def subir():
+    stringToSign= b""
+    stringToSign=base64.b64encode(bytes((policy).encode("utf-8")))
+
+    
+    signing_key = getSignatureKey(secret_key, dateStamp, region, service)
+    signature = hmac.new(signing_key, stringToSign, hashlib.sha256).hexdigest()
+    
+    #print(dateStamp)
+    #print(signature)
+    print(policy)
+    return {
+        'statusCode': 200,
+        'headers': { 'Access-Control-Allow-Origin' : '*' },
+        'body':json.dumps({ 'stringSigned' :  signature , 'stringToSign' : stringToSign.decode('utf-8') , 'xAmzCredential' : access_key+"/"+dateStamp+"/"+region+ "/s3/aws4_request" , 'dateStamp' : dateStamp , 'amzDate' : amzDate , 'securityToken' : securityToken })
+    }
+
+def nuevoVideo(usuarioId,tamanyo,nombre,descripcion):
+    conn = connect()
+    body = {}
+    try:
+        with conn.cursor() as cur:
+            cur.execute("insert into Video(usuarioId,tamanyo,nombre,descripcion) values("+usuarioId+","+tamanyo+","+nombre+","+descripcion+")")
+            conn.commit()
+            ok = cur.execute("select id from Video order by id desc limit 1")
+            conn.commit()
+            if (ok == 1):
+                body["id"] = cur.fetchone()[0]
+    except pymysql.MySQLError as e:    
+        print (e)
+        body["redirectPage"] = urlbase+"error.html"
+    return {
+        'statusCode': 200,
+        'headers': { 'Access-Control-Allow-Origin' : '*' },
+        'body' : json.dumps(body)
+    }
+
+def comentar(usuarioId,videoId,contenido,comentarioPadreId):
+    conn = connect()
+    body = {}
+    try:
+        with conn.cursor() as cur:
+            if comentarioPadreId != -1:
+                cur.execute("insert into Comentario(usuarioId,videoId,contenido,comentarioPadreId) values("+usuarioId+","+videoId+","+contenido+","+comentarioPadreId+")")
+            else:
+                cur.execute("insert into Comentario(usuarioId,videoId,contenido) values("+usuarioId+","+videoId+","+contenido+")")
+            conn.commit()
+            ok = cur.execute("select id from Comentario order by id desc limit 1")
+            conn.commit()
+            if (ok == 1):
+                body["id"] = cur.fetchone()[0]
+    except pymysql.MySQLError as e:    
+        print (e)
+        body["redirectPage"] = urlbase+"error.html"
     return {
         'statusCode': 200,
         'headers': { 'Access-Control-Allow-Origin' : '*' },
@@ -132,12 +195,33 @@ def lambda_handler(event , context):
     op=event["queryStringParameters"]["op"]
     
     if op == "login":
-        user=float(event["queryStringParameters"]["user"])
-        password=float(event["queryStringParameters"]["pass"])
-        return login()
+        usuario = event["queryStringParameters"]["usuario"]
+        contrasenya = event["queryStringParameters"]["contrasenya"]
+        return login(usuario,contrasenya)
     if op == "registarse":
-        return registrarse()
+        nombreUsuario = event["queryStringParameters"]["nombreUsuario"]
+        email = event["queryStringParameters"]["email"]
+        nombreCompleto = event["queryStringParameters"]["nombreCompleto"]
+        contrasenya = event["queryStringParameters"]["contrasenya"]
+        fraseRecuperacion = event["queryStringParameters"]["fraseRecuperacion"]
+        return registrarse(nombreUsuario,email,nombreCompleto,contrasenya,fraseRecuperacion)
     if op == "subir":
-        return {}
-    return {}
+        return subir()
+    if op == "nuevoVideo":
+        usuarioId = event["queryStringParameters"]["usuarioId"]
+        tamanyo = event["queryStringParameters"]["tamanyo"]
+        nombre = event["queryStringParameters"]["nombre"]
+        descripcion = event["queryStringParameters"]["descripcion"]
+        return nuevoVideo(usuarioId,tamanyo,nombre,descripcion)
+    if op == "comentar":
+        usuarioId = event["queryStringParameters"]["usuarioId"]
+        videoId = event["queryStringParameters"]["videoId"]
+        contenido = event["queryStringParameters"]["contenido"]
+        comentarioPadreId = event["queryStringParameters"]["comentarioPadreId"]
+        return comentar(usuarioId,videoId,contenido,comentarioPadreId)
+    return {
+        'statusCode': 200,
+        'headers': { 'Access-Control-Allow-Origin' : '*' },
+        'body' : json.dumps({ "redirectPage": urlbase+"error.html" })
+    }
 #      
